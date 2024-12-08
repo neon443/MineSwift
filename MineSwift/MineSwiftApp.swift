88a0b5826798d3dd8cf15baafb6b22cf4eb9ad06
neon443 <69979447+neon443@users.noreply.github.com>
2024-12-08 20:33:05 +0000

    Initial Commit

---
+++ MineSwift/MineSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineSwiftApp.swift
+//  MineSwift
+//
+//  Created by Nihaal Sharma on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MineSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MineSwiftTests/MineSwiftTests.swift
@@ -0,0 +1,16 @@
+//
+//  MineSwiftTests.swift
+//  MineSwiftTests
+//
+//  Created by Nihaal Sharma on 08/12/2024.
+//
+
+import Testing
+
+struct MineSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
